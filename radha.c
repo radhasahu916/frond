0f8bcefd10ded7c54b88ecf2aa5bb674a7bad2e8
radhasahu916 <radhasahu5657@gmail.com>
2025-08-19 15:03:54 +0530

    index.html

---
+++ radha.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+int main() {
+int a;
+int b;
+int c;
+int d;
+
+printf ("enter your first value ");
+scanf ("%d",&a);
+
+printf ("enter your second value");
+scanf (" %d",&b);
+
+printf ("enter your third value");
+scanf ("%d",&c);
+
+d=(a=b=c)/3;
+
+printf("average=");
+printf("%f,d");
+
+}
